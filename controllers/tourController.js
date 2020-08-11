3d0e78b8abbe170e96f2232a8abf060edf37dfe1
YoGi <yogeshmishra667@gmail.com>
2020-08-11 17:01:34 +0530

    added advance filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,20 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //BUILD
+    //1)filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    //ADVANCE FILTERING
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    const query = Tour.find(JSON.parse(queryStr));
+    //EXECUTE QUERY
+    const tours = await query;
+
     res.status(200).json({
       status: 'success',
       results: tours.length,
