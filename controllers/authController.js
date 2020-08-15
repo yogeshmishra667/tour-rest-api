2310319d04918f8ff44c1930ad3c5bd1498f3f37
YoGi <yogeshmishra667@gmail.com>
2020-08-15 22:49:43 +0530

    add jwy and generate token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,16 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
 
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_TIME
+  });
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser
     }
