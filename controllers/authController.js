7675a2784c2297d6157d4674594c3000aba8a246
YoGi <yogeshmishra667@gmail.com>
2020-08-16 18:29:41 +0530

    added authorization roles & permissions

--- controllers/authController.js
+++ controllers/authController.js
@@ -97,3 +97,18 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+//for user permission & authorization
+exports.restrictTo = (...roles) => {
+  //use closure because middleware () never take args
+  return (req, res, next) => {
+    // roles ['admin', 'lead-guide']. role='user'
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
+    }
+
+    next();
+  };
+};
