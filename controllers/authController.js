5c34233d4bf3935bc44ce7944a22dd24fded00f2
YoGi <yogeshmishra667@gmail.com>
2020-08-17 21:17:09 +0530

    send token by cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,18 @@ const signToken = id => {
 //FOR SEND TOKEN FOR ALL ENDPOINT (REUSE CODE)
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  //SEND TOKEN BY COOKIE
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000 //90 DAY
+    ),
+    httpOnly: true
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from output
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
