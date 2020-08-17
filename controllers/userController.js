f7c28e7cd645ce89a829414a83f4a413d38a200c
YoGi <yogeshmishra667@gmail.com>
2020-08-17 18:15:25 +0530

    login user update own data like name-email

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,17 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
-//const AppError = require('./../utils/appError');
+const AppError = require('./../utils/appError');
+
+//for filter data because if we can't filter data someone change sensitive data
+const filterObj = (obj, ...allowedFields) => {
+  //allowedFields take name, email
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+    //if any fields includes only value of of original obj(obj) copy in newObj
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -14,6 +25,37 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     }
   });
 });
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    //we can't user save method because it show validation error for other fields
+    //we want to change only require data not all data
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,6 +14,7 @@ router.patch(
   authController.protect,
   authController.updatePassword
 );
+router.patch('/updateMe', authController.protect, userController.updateMe);
 
 router
   .route('/')
