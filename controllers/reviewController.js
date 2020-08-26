19e1abf38032097ab6184bc43a09563712629e72
YoGi <yogeshmishra667@gmail.com>
2020-08-26 14:12:09 +0530

    added merge params for review and tour

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -21,6 +21,9 @@ exports.getAllReview = catchAsync(async (req, res, next) => {
 
 //for creating new review
 exports.createReview = catchAsync(async (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
   const newReview = await Review.create(req.body);
 
   res.status(200).json({
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewRouter = require('../routes/reviewRoutes');
 
 const router = express.Router();
+
+//for marge review router
+router.use('/:tourId/reviews', reviewRouter);
 
 // router.param('id', tourController.checkID);
 router
