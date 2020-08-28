c16606e9e6850e2357850c74c5cfecb0e871d646
YoGi <yogeshmishra667@gmail.com>
2020-08-28 14:48:01 +0530

    calculate average rating on tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = mongoose.Schema(
   {
@@ -41,6 +42,43 @@ reviewSchema.pre(/^find/, function(next) {
   });
   next();
 });
+
+//FOR CALCULATE AVERAGE RATING
+//use static method because want to point current model using this
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+  // console.log(stats);
+
+  //if (stats.length > 0) {
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating
+  });
+  // } else {
+  //   await Tour.findByIdAndUpdate(tourId, {
+  //     ratingsQuantity: 0,
+  //     ratingsAverage: 4.5
+  //   });
+  // }
+};
+
+reviewSchema.post('save', function() {
+  // this points to current
+  //constructor point the current docs who is created
+  this.constructor.calcAverageRatings(this.tour);
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
