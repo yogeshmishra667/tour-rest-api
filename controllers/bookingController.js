77d5dc05fe7d7ff321e667093c9670e90699e951
YoGi <yogeshmishra667@gmail.com>
2020-09-12 12:57:29 +0530

    added booking features with payment integration using stripe I

---
+++ controllers/bookingController.js
@@ -0,0 +1,38 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Tour = require('../models/tourModel');
+//const Booking = require('../models/bookingModel');
+const catchAsync = require('../utils/catchAsync');
+//const factory = require('./handlerFactory');
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  // 1) Get the currently booked tour
+  const tour = await Tour.findById(req.params.tourId);
+  console.log(tour);
+
+  // 2) Create checkout session
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
+      req.params.tourId
+    }`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourId,
+    line_items: [
+      {
+        name: `${tour.name} Tour`,
+        description: tour.summary,
+        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        amount: tour.price * 100,
+        currency: 'inr',
+        quantity: 1
+      }
+    ]
+  });
+
+  // 3) Create session as response
+  res.status(200).json({
+    status: 'success',
+    session
+  });
+});
