21d9284cc4b47a950518567e4216909eb8070c52
YoGi <yogeshmishra667@gmail.com>
2020-09-13 14:40:34 +0530

    complete booking crud operation using handler function ( modern way)

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -2,8 +2,8 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
 const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
-//const factory = require('./handlerFactory');
+//const AppError = require('../utils/appError');
+const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
@@ -49,82 +49,8 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
   res.redirect(req.originalUrl.split('?')[0]);
 });
 
-//FETCH ALL BOOKING
-exports.getAllBookings = catchAsync(async (req, res, next) => {
-  const booking = await Booking.find();
-
-  if (!booking) {
-    return next(new AppError('No booking found with that info', 404));
-  }
-
-  res.status(200).json({
-    status: 'success',
-    results: booking.length,
-    data: {
-      data: booking
-    }
-  });
-});
-
-//CREATE BOOKING
-exports.createBooking = catchAsync(async (req, res, next) => {
-  const booking = await Booking.create(req.body);
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      data: booking
-    }
-  });
-});
-
-//FETCH SINGLE BOOKING
-exports.getBooking = catchAsync(async (req, res, next) => {
-  const booking = await Booking.findById(req.params.id);
-
-  if (!booking) {
-    return next(new AppError('No booking found with that ID', 404));
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      data: booking
-    }
-  });
-});
-
-//UPDATE BOOKING
-exports.updateBooking = catchAsync(async (req, res, next) => {
-  const booking = await Booking.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true
-  });
-
-  if (!booking) {
-    return next(new AppError('No booking found with that ID', 404));
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      data: booking
-    }
-  });
-});
-
-//DELETE BOOKING
-exports.deleteBooking = catchAsync(async (req, res, next) => {
-  const booking = await Booking.findByIdAndRemove(req.params.id);
-
-  if (!booking) {
-    return next(new AppError('No booking found with that ID', 404));
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      data: null
-    }
-  });
-});
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
