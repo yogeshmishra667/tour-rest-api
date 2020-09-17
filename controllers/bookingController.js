d8cdf274c8c7472690dff75a0e463d6dca1b51d7
YoGi <yogeshmishra667@gmail.com>
2020-09-17 12:37:48 +0530

    fix error

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -19,7 +19,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     //   req.params.tourId
     // }&user=${req.user.id}&price=${tour.price}`,
     //SUCCESSURL WITHOUT STRIPE
-    success_url: `${req.protocol}://${req.get('host')}/my-tours/`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const viewController = require('../controllers/viewController');
 const authController = require('../controllers/authController');
-const bookingController = require('../controllers/bookingController');
+//const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
 //FOR OVERVIEW/HOME PAGE
-router.get(
-  '/',
-  bookingController.createBookingCheckout, //because after payment success redirect at home route
-  authController.isLoggedIn,
-  viewController.getOverview
-);
+router.get('/', authController.isLoggedIn, viewController.getOverview);
 //FOR TOUR PAGE
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 //FOR LOGIN USER
